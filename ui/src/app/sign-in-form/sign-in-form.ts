72aa79c73e8fb40356fdc60ce6d58b74d8dd8a76
hamstersnore <hamstersnore@gmail.com>
2026-07-24 23:57:16 +0200

    refactor on sign in form style to show only black whites and grays. Initial work on form model and submit logic

--- ui/src/app/sign-in-form/sign-in-form.ts
+++ ui/src/app/sign-in-form/sign-in-form.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
+
+interface SignInModel {
+  Username: string;
+  Password: string;
+}
 
 @Component({
   selector: 'app-sign-in-form',
-  imports: [],
+  imports: [FormField],
   templateUrl: './sign-in-form.html',
   styleUrl: './sign-in-form.css',
 })
 export class SignInForm {
+
+  signInModel = signal<SignInModel>({
+    Username: '',
+    Password: ''
+  })
+
+  signInForm = form(this.signInModel)
+
+  onSubmit(event:Event){
+    console.log(this.signInModel().Username, this.signInModel().Password)
+    // POST /api/auth/sign-in
+  }
 
 }
