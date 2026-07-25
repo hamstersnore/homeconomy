3704bc4dcad813f44a08ce8778514c6c30cbfe03
hamstersnore <hamstersnore@gmail.com>
2026-07-25 23:52:00 +0200

    Dashboard route, screen and preliminar design with mock data

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { SignUpForm } from './sign-up-form/sign-up-form';
 import { SignInForm } from './sign-in-form/sign-in-form';
 import { CreateTransaction } from './create-transaction/create-transaction';
 import { GetTransactions } from './get-transactions/get-transactions'
+import { Dashboard } from './dashboard/dashboard';
 
 export const routes: Routes = [
     { path: 'sign-up', component: SignUpForm },
     { path: 'sign-in', component: SignInForm },
     { path: 'create-transaction', component: CreateTransaction },
-    { path: 'transactions', component: GetTransactions }
+    { path: 'transactions', component: GetTransactions },
+    { path: 'dashboard', component: Dashboard }
 ];
