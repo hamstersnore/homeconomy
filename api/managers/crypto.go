15f78d97e68c2afeba12923d3c012fa78a59d5bf
hamstersnore <hamstersnore@gmail.com>
2026-08-03 08:24:58 +0200

    jwt refactor.  GetClaims function finished

--- api/managers/crypto.go
+++ api/managers/crypto.go
@@ -1,11 +1,6 @@
 package managers
 
 import (
-	"log"
-	"os"
-	"time"
-
-	"github.com/golang-jwt/jwt/v5"
 	_ "github.com/joho/godotenv/autoload"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -22,34 +17,4 @@ func HashPassword(passwd string) []byte {
 
 func CheckPassword(passwd string, hashedPasswd []byte) bool {
 	return bcrypt.CompareHashAndPassword(hashedPasswd, []byte(passwd)) == nil
-}
-
-func GenerateJwt(userId int, username string) string {
-	key := os.Getenv("SIGNING_KEY")
-	log.Printf("Key from .env -> %s\n", key)
-	signingKey := []byte(key)
-
-	type HomeconomyClaims struct {
-		Id       int    `json:"id"`
-		Username string `json:"username"`
-		jwt.RegisteredClaims
-	}
-
-	claims := HomeconomyClaims{
-		Id:       userId,
-		Username: username,
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-			NotBefore: jwt.NewNumericDate(time.Now()),
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	ss, err := token.SignedString(signingKey)
-	log.Printf("jwt -> %s\n", ss)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return ss
 }
---
+++ api/managers/jwt.go
@@ -0,0 +1,55 @@
+package managers
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type HomeconomyClaims struct {
+	Id       int    `json:"id"`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJwt(userId int, username string) string {
+	key := os.Getenv("SIGNING_KEY")
+	log.Printf("Key from .env -> %s\n", key)
+	signingKey := []byte(key)
+
+	claims := HomeconomyClaims{
+		Id:       userId,
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(signingKey)
+	log.Printf("jwt -> %s\n", ss)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ss
+}
+
+func GetClaims(r *http.Request) *HomeconomyClaims {
+	authHeader := r.Header.Get("Authorization")
+	token, err := jwt.ParseWithClaims(authHeader, &HomeconomyClaims{}, func(token *jwt.Token) (any, error) {
+		return []byte(os.Getenv("SIGNING_KEY")), nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	claims := token.Claims.(*HomeconomyClaims)
+
+	return claims
+}
